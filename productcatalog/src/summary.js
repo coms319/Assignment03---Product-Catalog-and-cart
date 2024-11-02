3478d4f3cb56e002cfcb48fcab7c3768507a48de
LucasNeroneRillo <lneronerillo@gmail.com>
2024-11-02 14:25:03 -0500

    Debugged summary

--- productcatalog/src/summary.js
+++ productcatalog/src/summary.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Navbar, Nav, Button, Container, Row } from 'react-bootstrap';
+import { IoChevronBackCircleSharp } from 'react-icons/io5';
 
 const Summary = ({ viewer, setViewer }) => {
+  const handleBackwardsClick = () => {
+    setViewer(1);
+  };
+
   return (
     <>
       {viewer === 2 && (
         <>
-          <div>Hello world</div>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Nav className="me-auto">
+                <Button variant="outline-light" onClick={handleBackwardsClick}>
+                  <IoChevronBackCircleSharp /> Cart
+                </Button>
+              </Nav>
+            </Container>
+          </Navbar>
+          <Container className="mt-4">
+            <Row>TODO</Row>
+          </Container>
         </>
       )}
     </>
